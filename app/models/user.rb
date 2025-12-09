ca24cf706bd9b20356f2f5df10673f102b6e6808
CristinaLazar12 <cristinalazar122@gmail.com>
2025-12-09 11:31:49 +0200

    Model tests working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email_address, presence: true, uniqueness: true
+
   has_secure_password
 
   has_many :sessions, dependent: :destroy
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -2,23 +2,36 @@ require "test_helper"
 
 class TripTest < ActiveSupport::TestCase
   test "invalid without a name" do
-    trip = Trip.new
-    assert_not trip.valid?
+    trip = Trip.new #tripul e invalid fara un nume, deci nu trecem aici nume
+    assert trip.invalid? #tripul este invalid
     assert_includes trip.errors[:name], "can't be blank"
   end
 
-  test "invalid without a user" do
-    trip = Trip.new
-    assert_not trip.valid?
-    assert_includes trip.errors[:user], "can't be blank"
+  test "invalid without a creator" do
+    trip = Trip.new(name: "First Trip") #tripul e invalid fara creator, asa ca ii adaugam nume si nu si creator
+    assert trip.invalid? #tripul este invalid
+    assert_includes trip.errors[:creator], "must exist"
   end
 
-  test "create a trip" do
-    current_user = User.create(name: "Test User",
-                              email: "test@example.com",
-                              email_address: "test@example.com", 
-                              password: "password")
-    trip = Trip.new(user: current_user, name: "Test trip")
+  test "valid with a name and creator" do
+    creator = User.create(email_address: "test@test.com", password: "password")
+    trip = Trip.new(name: "Test trip", creator: creator)
+
     assert trip.valid?
+  end
+
+  test "can have participants" do
+    creator = User.create(name: "User1", email_address: "test@test.com", password: "password")
+    trip = Trip.new(name: "Test trip", creator: creator)
+
+    participant1 = User.create(name: "User2", email_address: "test1@test.com", password: "password1")
+    participant2 = User.create(name: "User3", email_address: "test2@test.com", password: "password2")
+
+    trip.users << participant1
+    trip.users << participant2
+
+    assert trip.valid?
+    assert_includes trip.users, participant1
+    assert_includes trip.users, participant2
   end
 end

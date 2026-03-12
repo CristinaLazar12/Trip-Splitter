9b9b84412a5d63da332924c24a7dacc53284a36f
CristinaLazar12 <cristinalazar122@gmail.com>
2026-03-11 23:26:50 -0700

    some expense controller tests written

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,10 @@
 class ExpensesController < ApplicationController
+
+  def index
+    @trip = Trip.find(params[:trip_id])
+    @expenses = @trip.expenses
+  end
+
   def new
     @trip = Trip.find(params[:trip_id])
     @expense = Expense.new
@@ -9,15 +15,20 @@ class ExpensesController < ApplicationController
     @expense = Expense.new(expense_params)
 
     if @expense.save
-      redirect_to @trip
+      redirect_to trip_expenses_url(@trip)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+  
+  def show
+    @trip = Trip.find(params[:trip_id])
+    @expenses = @trip.expenses
   end
 
   private
 
   def expense_params
-    params.require(:expense).permit(:title, :amount, :currency, :date)
+    params.require(:expense).permit(:title, :amount, :currency, :date, :split_type, :trip_id, :payer_id)
   end
 end
--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class ExpensesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @trip  = trips(:one)
+    @user  = users(:one)
+    @non_creator = users(:two) 
+    @expense = expenses(:one)     
+  end
+
+  test "should get index" do
+    sign_in_as @user #se logheaza ca si creator
+    get trip_expenses_url(@trip)
+    assert_response :success
+  end
+
+  test "should get new" do
+    sign_in_as @user #se logheaza ca si creator
+    get new_trip_expense_url(@trip)
+    assert_response :success
+  end
+
+  test "should create expense" do
+    sign_in_as @user 
+
+    assert_difference("Expense.count") do
+        post trip_expenses_url(@trip), params: { expense: {
+            title: "Lunch",
+            amount: 50,
+            currency: "RON",
+            date: Date.new(2025, 10, 11),
+            split_type: "equal",
+            payer_id: @user.id 
+        } }
+        end
+
+        assert_redirected_to trip_expense_url(Expense.last)
+  end
+
+  test "should show expense" do
+    sign_in_as @user #se logheaza ca si creator
+    get trip_expense_url(@trip, @expense)
+    assert_response :success
+  end
 end

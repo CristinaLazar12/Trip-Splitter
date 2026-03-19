0fe80027798bb447ba23a23403f90a45fc34df08
CristinaLazar12 <cristinalazar122@gmail.com>
2026-03-18 18:32:50 -0700

    Payer dropdown added

--- app/policies/expense_policy.rb
+++ app/policies/expense_policy.rb
@@ -2,47 +2,47 @@ class ExpensePolicy < ApplicationPolicy
     attr_reader :user, :expense
 
     def initialize(user, expense)
-        @user = user
-        @expense = expense
+      @user = user
+      @expense = expense
     end
 
     def edit?
-        can_manage_expense?
+      can_manage_expense?
     end
 
     def update?
-        can_manage_expense?
+      can_manage_expense?
     end
 
     def destroy?
-        can_manage_expense?
+      can_manage_expense?
     end
 
     def create?
-        trip_participant?
+      trip_participant?
     end
 
     private
 
     def trip_participant?
-        expense.trip.participants.include?(user)
+      expense.trip.participants.include?(user)
     end
 
     def can_manage_expense?
-        trip_creator? ||
-        expense_creator? ||
-        expense_payer?
+      trip_creator? ||
+      expense_creator? ||
+      expense_payer?
     end
 
     def trip_creator?
-        expense.trip.creator_id == user.id
+      expense.trip.creator == user
     end
 
     def expense_creator?
-        expense.creator_id == user.id
+      expense.creator == user
     end
 
     def expense_payer?
-        expense.payer_id == user.id
+      expense.payer == user
     end
 end
